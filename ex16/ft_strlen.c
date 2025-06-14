1aca508fcbe5945149f99b0dfab9c563c841dc9d
urabexon <hiroki708.a48uk@gmail.com>
2025-06-14 09:25:04 +0900

    add

--- ex16/ft_strlen.c
+++ ex16/ft_strlen.c
@@ -6,9 +6,18 @@
 /*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 21:36:53 by urabex            #+#    #+#             */
-/*   Updated: 2025/06/07 21:40:55 by urabex           ###   ########.fr       */
+/*   Updated: 2025/06/14 09:24:57 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    char *input;
+    int   expected;
+} TestCase;
 
 int ft_strlen(char *str);
 
@@ -22,4 +31,31 @@ int ft_strlen(char *str)
     while (str[i] != '\0')
         i++;
     return (i);
+}
+
+int main(void)
+{
+    TestCase tests[] = {
+        { "",              0 },
+        { "Hello, World!", 13 },
+        { "42Tokyo",        7 },
+        { "Test\0Hidden",   4 },
+        { NULL,             0 }, 
+        { NULL,             0 }
+    };
+
+    for (int i = 0; tests[i].input != NULL || i == 4; i++) {
+        char *s = tests[i].input;
+        int   expect = tests[i].expected;
+        int   actual = ft_strlen(s);
+        printf("Test %d: ", i);
+        if (s)
+            printf("\"%s\"", s);
+        else
+            printf("NULL");
+        printf(" → expected=%d, actual=%d : %s\n",
+               expect, actual,
+               (expect == actual) ? "OK" : "NG");
+    }
+    return 0;
 }
